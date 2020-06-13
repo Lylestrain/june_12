aef971de3a856debc15baa9c1ec9907781951998
Lyle Strain <lylestrain@gmail.com>
2020-06-12 23:08:33 -0400

    Update main.js

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+function changeColour() {
+    let red = document.getElementById('rangeRed').value;
+    let green = document.getElementById('rangeGreen').value;
+    let blue = document.getElementById('rangeBlue').value;
+    let colour = 'rgb(' + red + ',' + green + ',' + blue + ')';
+    document.body.style.backgroundColor = colour ;
+    document.getElementById('colourOutput').innerHTML = ':' + colour;
+}
+
+document.getElementById('rangeRed').addEventListener('click' , changeColour);
+document.getElementById('rangeGreen').addEventListener('click' , changeColour);
+document.getElementById('rangeBlue').addEventListener('click' , changeColour);
